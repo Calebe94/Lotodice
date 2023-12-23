828d6f5e70e7e8a5e4fe5bb84149410fb3ad1f58
Calebe94 <calebe94@disroot.org>
2023-12-23 00:12:21 -0300

    Added game generator script

---
+++ main.py
@@ -0,0 +1,68 @@
+import sys
+import csv
+import random
+
+def gerar_jogo():
+    numeros = random.sample(range(1, 61), 6)
+    numeros.sort()
+    return numeros
+
+def gerar_jogos(qtde_jogos):
+    jogos = []
+
+    for _ in range(qtde_jogos):
+        jogos.append(gerar_jogo())
+
+    return jogos
+
+def verificar_premio(jogos, numeros_sorteados):
+    for i, jogo in enumerate(jogos, start=1):
+        acertos = set(jogo).intersection(numeros_sorteados)
+        print(f"Jogo {i}: {', '.join(map(str, jogo))} - Acertos: {len(acertos)}")
+        if len(acertos) == 6:
+            print("----------------------------")
+            print("GANHAMOOOOOO CARALHOOOOOW!!!")
+            print("----------------------------")
+
+def carregar_jogos(arquivo):
+    jogos = []
+    with open(arquivo, 'r') as csvfile:
+        leitor = csv.reader(csvfile)
+        for linha in leitor:
+            jogo = [int(numero) for numero in linha]
+            jogos.append(jogo)
+    return jogos
+
+def exportar_para_csv(jogos, path):
+    with open(path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for jogo in jogos:
+            writer.writerow(jogo)
+
+def main():
+    if len(sys.argv) != 4:
+        print("Uso: python nome_do_arquivo.py [quantidade_de_jogos | -export caminho_para_arquivo.csv | -check 'numeros_sorteados' caminho_para_arquivo.csv]")
+        sys.exit(1)
+
+    if sys.argv[1] == '-check':
+        numeros_sorteados = list(map(int, sys.argv[2].split(',')))  # Remove aspas e converte para lista de inteiros
+        arquivo_jogos = sys.argv[3]
+        jogos = carregar_jogos(arquivo_jogos)
+        print(f"Números sorteados: {', '.join(map(str, numeros_sorteados))}")
+        verificar_premio(jogos, numeros_sorteados)
+
+    elif sys.argv[1] == '-export':
+        quantidade_de_jogos = int(sys.argv[3])
+        resultados = gerar_jogos(quantidade_de_jogos)
+        caminho_arquivo = sys.argv[2]
+        exportar_para_csv(resultados, caminho_arquivo)
+        print(f"Jogos exportados para {caminho_arquivo}")
+
+    else:
+        quantidade_de_jogos = int(sys.argv[1])
+        resultados = gerar_jogos(quantidade_de_jogos)
+        for i, jogo in enumerate(resultados, start=1):
+            print(f"Jogo {i}: {', '.join(map(str, jogo))}")
+
+if __name__ == '__main__':
+    main()
